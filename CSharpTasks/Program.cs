acb78c108b78f6ff58d9df4efbd6076bdd18ebae
Max Pavlow <lunisori@mail.ru>
2024-07-20 14:07:17 +0500

    Добавлен счетчик количества символов в результирующей строке

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -2,8 +2,21 @@
 {
     string? input = Console.ReadLine();
     string result;
+    bool error = false;
 
     if (input == null) return;
+
+    foreach (char symbol in input)
+    {
+        if (symbol < 'a'  ||  symbol > 'z')
+        {
+            Console.WriteLine("{0} - некорректный символ!", symbol);
+            error = true;
+        }
+    }
+
+    if (error) return;
+
 
     if (input.Length % 2 == 0)
     {
@@ -21,4 +34,19 @@
     };
 
     Console.WriteLine(result);
+
+    var unique = result.Distinct();
+
+    for (int i = 0; i < unique.Count(); i++)
+    {
+        int counter = 0;
+        for (int j = 0; j < result.Length; j++)
+        {
+            if (unique.ToList()[i] == result[j])
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine("Символ \"{0}\" повторялся {1} раз", unique.ToList()[i], counter);
+    }
 }
